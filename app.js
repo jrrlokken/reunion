33e794b1c9800f17d3937021144a5dc7b0cfed20
Joshua Lokken <joshualokken@pm.me>
2022-03-23 22:58:52 -0500

    Configured multer for multiple file upload

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/png' ||
     file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/webp'
   ) {
     cb(null, true);
   } else {
@@ -54,8 +55,11 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(
+//   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+// );
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).array('images', 8)
 );
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
