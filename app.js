1e5513be5234b2724d48778f0dab7e26716702e0
Joshua Lokken <joshualokken@pm.me>
2022-03-26 12:46:06 +0000

    Small tweaks

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+// const cloudinary = require('cloudinary').v2;
 
-if (typeof process.env.CLOUDINARY_URL === 'undefined') {
-  console.warn('!! Cloudinary config is undefined');
-  console.warn('export CLOUDINARY_URL or set dotenv file');
-} else {
-  console.log('** Cloudinary config:');
-  console.log(cloudinary.config());
-}
+// if (typeof process.env.CLOUDINARY_URL === 'undefined') {
+//   console.warn('!! Cloudinary config is undefined');
+//   console.warn('export CLOUDINARY_URL or set dotenv file');
+// } else {
+//   console.log('** Cloudinary config:');
+//   console.log(cloudinary.config());
+// }
 
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const multer = require('multer');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 const store = new MongoDBStore({
   uri: process.env.MONGODB_URI,
-  collection: 'sessions',
+  collection: "sessions",
 });
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './images');
+    cb(null, "./images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, new Date().toISOString() + "-" + file.originalname);
     // cb(
     //   null,
     //   new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname
@@ -44,10 +44,10 @@ const fileStorage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/webp'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/webp"
   ) {
     cb(null, true);
   } else {
@@ -55,23 +55,23 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const indexRoute = require('./routes/index');
-const reunionRoutes = require('./routes/reunion');
-const authRoutes = require('./routes/auth');
-const adminRoutes = require('./routes/admin');
+const indexRoute = require("./routes/index");
+const reunionRoutes = require("./routes/reunion");
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(
 //   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 // );
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).array('images', 8)
+  multer({ storage: fileStorage, fileFilter: fileFilter }).array("images", 8)
 );
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
   session({
@@ -108,19 +108,19 @@ app.use((req, res, next) => {
     });
 });
 
-app.use('/admin', adminRoutes);
-app.use('/reunions', reunionRoutes);
+app.use("/admin", adminRoutes);
+app.use("/reunions", reunionRoutes);
 app.use(indexRoute);
 app.use(authRoutes);
 
-app.use('/500', errorController.get500);
+app.use("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   console.log(error);
-  res.status(500).render('500', {
-    pageTitle: 'Server Error',
-    path: '/500',
+  res.status(500).render("500", {
+    pageTitle: "Server Error",
+    path: "/500",
     isAuthenticated: req.session.isLoggedIn,
   });
 });
@@ -128,7 +128,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((result) => {
-    app.listen(3000);
-    console.log('Database connected');
+    app.listen(process.env.PORT);
+    console.log("Database connected");
   })
   .catch((error) => console.log(error));
