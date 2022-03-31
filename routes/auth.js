74db94b20b6d4aa1f03dbe9e51a8b85faecaed03
Joshua Lokken <joshualokken@pm.me>
2022-03-30 21:50:27 -0500

    Implement favicon

--- routes/auth.js
+++ routes/auth.js
@@ -2,38 +2,39 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
-// router.get('/signup', authController.getSignup);
-// router.post(
-//   '/signup',
-//   body('email')
-//     .isEmail()
-//     .withMessage('Please enter a valid email address')
-//     .custom((value, { req }) => {
-//       return User.findOne({ email: value }).then((userDoc) => {
-//         if (userDoc) {
-//           return Promise.reject('Account exists with that email');
-//         }
-//       });
-//     })
-//     .normalizeEmail(),
-//   body('password', 'Password must be at least 8 characters')
-//     .isLength({
-//       min: 8,
-//     })
-//     .trim(),
-//   body('confirmPassword')
-//     .trim()
-//     .custom((value, { req }) => {
-//       if (value !== req.body.password) {
-//         throw new Error('Passwords must match');
-//       }
-//       return true;
-//     }),
-//   authController.postSignup
-// );
+router.get('/signup', authController.getSignup);
+router.post(
+  '/signup',
+  body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email address')
+    .custom((value, { req }) => {
+      return User.findOne({ email: value }).then((userDoc) => {
+        if (userDoc) {
+          return Promise.reject('Account exists with that email');
+        }
+      });
+    })
+    .normalizeEmail(),
+  body('password', 'Password must be at least 8 characters')
+    .isLength({
+      min: 8,
+    })
+    .trim(),
+  body('confirmPassword')
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords must match');
+      }
+      return true;
+    }),
+  authController.postSignup
+);
 
 router.get('/login', authController.getLogin);
 router.post(
