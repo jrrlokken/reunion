c11f73107b117a39dd4e134c0460201e35d7e479
Joshua Lokken <joshualokken@pm.me>
2022-03-30 21:43:46 -0500

    Merge

--- controllers/reunion.js
+++ controllers/reunion.js
@@ -42,9 +42,9 @@ exports.getUpcoming = (req, res, next) => {
       title: 'Upcoming Lokken Reunion!',
       year: 2023,
       imageUrl:
-        'https://images.unsplash.com/photo-1559014157-33373e15e6ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      description: `Here will be a description and details for the upcoming reunion.
-         Since we are not sure when it will take place, I'm just calling it the 2023 reunion for now 😉`,
+        'https://images.unsplash.com/photo-1562584086-7c6b531e01c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
+      description:
+        "Here will be a description of the upcoming reunion.  Since we are not sure when it will take place, I'm just calling it the 2023 reunion for now :)",
     },
   });
 };
