875c1ae20635e73f04fd4aeda03a2b01d6267eef
Joshua Lokken <joshualokken@pm.me>
2022-04-07 17:07:10 +0000

    Comment style updates

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,10 +1,10 @@
 // Comments
-const reunionId = document.getElementById('reunionId').value;
-const commentForm = document.getElementById('comment-form');
-const commentInput = document.getElementById('newComment');
-const commentsContainer = document.getElementById('allComments');
-let commentText = document.getElementById('newComment').value;
-const csrfToken = document.getElementById('csrf').value;
+const reunionId = document.getElementById("reunionId").value;
+const commentForm = document.getElementById("comment-form");
+const commentInput = document.getElementById("newComment");
+const commentsContainer = document.getElementById("allComments");
+let commentText = document.getElementById("newComment").value;
+const csrfToken = document.getElementById("csrf").value;
 
 // Enable pusher logging - don't include this in production
 Pusher.logToConsole = false;
@@ -13,11 +13,11 @@ function buildComment(comment) {
   // const d = comment.createdAt.getDate();
   // const m = monthNames[comment.createdAt.getMonth()];
   // const y = comment.createdAt.getFullYear();
-  const commentDiv = document.createElement('div');
-  const commentP = document.createElement('p');
-  commentP.classList.add('comment-header-text');
+  const commentDiv = document.createElement("div");
+  const commentP = document.createElement("p");
+  commentP.classList.add("comment-header-text");
   const email = comment.userId.email;
-  const hr = document.createElement('hr');
+  const hr = document.createElement("hr");
   // commentP.textContent = `On ${m}+ ' ' +${d}+ ', ' +${y}, ${email} wrote:`;
   commentP.textContent = `${email} wrote:`;
   commentDiv.append(commentP);
@@ -27,32 +27,32 @@ function buildComment(comment) {
   return commentForm.reset();
 }
 
-const pusher = new Pusher('369474ee4c2e0ecdb50c', {
-  cluster: 'us2',
+const pusher = new Pusher("369474ee4c2e0ecdb50c", {
+  cluster: "us2",
 });
 
 const channel = pusher.subscribe(`${reunionId}`);
-channel.bind('comment', (data) => {
+channel.bind("comment", (data) => {
   const newComment = data.message;
   buildComment(newComment);
 });
 
-commentForm.addEventListener('submit', handleCommentSubmit, false);
-commentInput.addEventListener('change', (event) => {
+commentForm.addEventListener("submit", handleCommentSubmit, false);
+commentInput.addEventListener("change", (event) => {
   commentText = event.target.value;
 });
 
 async function handleCommentSubmit(event) {
   event.preventDefault();
 
-  const url = `http://localhost:3006/reunions/${reunionId}`;
+  const url = `https://www.lokkenreunion.com/reunions/${reunionId}`;
 
   fetch(url, {
-    method: 'post',
-    credentials: 'include',
+    method: "post",
+    credentials: "include",
     headers: {
-      'X-CSRF-Token': csrfToken,
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      "X-CSRF-Token": csrfToken,
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     },
     body: `commentText=${commentText}`,
   })
