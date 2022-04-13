216321a0a37af43a04886a1ccbf37f713e8851a3
Joshua Lokken <joshualokken@pm.me>
2022-04-13 21:34:07 +0000

    Updat

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
   const id = process.env.ADMIN_ID;
   if (!req.session) {
-    return res.redirect('/login');
-  } else if (req.session.user._id.toString() !== id) {
-    return res.redirect('/login');
+    return res.redirect("/login");
+  } else if (req.session.user._id.toString() !== id.toString()) {
+    return res.redirect("/login");
   }
   next();
 };
