498e214cf7ab87aedc68d1100360e2277ccd0178
Joshua Lokken <joshualokken@pm.me>
2022-09-21 11:13:21 -0500

    Add close nav on clickaway

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,11 +1,22 @@
 const openButton = document.querySelector('.open-btn');
 const closeButton = document.querySelector('.close-btn');
-const nav = document.querySelectorAll('.nav');
+const nav = document.querySelector('nav');
+const navs = document.querySelectorAll('.nav');
 
 openButton.addEventListener('click', () => {
-  nav.forEach((nav_el) => nav_el.classList.add('visible'));
+  navs.forEach((nav_el) => nav_el.classList.add('visible'));
 });
 
-closeButton.addEventListener('click', () => {
-  nav.forEach((nav_el) => nav_el.classList.remove('visible'));
+// closeButton.addEventListener('click', () => {
+//   navs.forEach((nav_el) => nav_el.classList.remove('visible'));
+// });
+
+document.addEventListener('click', (event) => {
+  if (
+    (!event.target.classList.contains('nav') &&
+      !event.target.classList.contains('fa-bars')) ||
+    event.target.classList.contains('close-btn')
+  ) {
+    navs.forEach((nav_el) => nav_el.classList.remove('visible'));
+  }
 });
